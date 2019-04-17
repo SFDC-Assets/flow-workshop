0e90ff8f633a39ed89da3e47cab954954e92e005
mshanemc <shane.mclaughlin@salesforce.com>
2019-04-17 16:19:24 -0500

    schema for translations object

--- orgInit.sh
+++ orgInit.sh
@@ -2,4 +2,4 @@ sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
 sfdx force:source:push
 sfdx force:user:permset:assign -n Courses
 sfdx force:data:bulk:upsert -s course__c -f data/courses.csv -i Id -w 5
-sfdx force:org:open
+sfdx force:org:open -p /lightning/o/Course__c/list?filterName=All_Courses
