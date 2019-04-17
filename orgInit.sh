5ca84648f483b26ea371d229be63f9ede81c71ba
mshanemc <shane.mclaughlin@salesforce.com>
2019-04-17 16:20:03 -0500

    don't wait on batch

--- orgInit.sh
+++ orgInit.sh
@@ -1,5 +1,5 @@
 sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
 sfdx force:source:push
 sfdx force:user:permset:assign -n Courses
-sfdx force:data:bulk:upsert -s course__c -f data/courses.csv -i Id -w 5
+sfdx force:data:bulk:upsert -s course__c -f data/courses.csv -i Id
 sfdx force:org:open -p /lightning/o/Course__c/list?filterName=All_Courses
