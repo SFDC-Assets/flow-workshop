d6df93c5a2faff2a62510c8d5938f35bef792993
mshanemc <shane.mclaughlin@salesforce.com>
2019-04-18 07:42:29 -0500

    with chatter group

--- orgInit.sh
+++ orgInit.sh
@@ -3,3 +3,4 @@ sfdx force:source:push
 sfdx force:user:permset:assign -n Courses
 sfdx force:data:bulk:upsert -s course__c -f data/courses.csv -i Id
 sfdx force:org:open -p /lightning/o/Course__c/list?filterName=All_Courses
+sfdx force:data:record:create -s CollaborationGroup -v "Name='Professional Translators' CollaborationType=Public"
