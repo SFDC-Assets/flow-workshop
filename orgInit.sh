c8b80288b4f6a6197fd9f178b401fdf798f46b23
mshanemc <shane.mclaughlin@salesforce.com>
2019-04-30 16:38:15 -0500

    basic commit

--- orgInit.sh
+++ orgInit.sh
@@ -3,4 +3,6 @@ sfdx force:source:push
 sfdx force:user:permset:assign -n Courses
 sfdx force:data:bulk:upsert -s course__c -f data/courses.csv -i Id
 sfdx force:org:open -p /lightning/o/Course__c/list?filterName=All_Courses
-sfdx force:data:record:create -s CollaborationGroup -v "Name='Professional Translators' CollaborationType=Public"
+sfdx force:data:record:create -s CollaborationGroup -v "Name='Professional Translators' CollaborationType=Public Description='We translate anything you pay us to.  Any language, we can make it happen.  No fictional languages (Klingon, Pig Latin, Elvish, etc), please.' InformationTitle='How Translation Works' InformationBody='πώς λειτουργεί η μετάφραση'"
+sfdx shane:group:photo -f assets/rosetta.jpg -g "Professional Translators"
+sfdx shane:group:photo -b assets/rosettabanner.jpg -g "Professional Translators"
