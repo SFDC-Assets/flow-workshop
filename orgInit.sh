6647bd39deceae33c3990880cade6f6c29e0bc72
Dave Scruggs <dscruggs@salesforce.com>
2021-03-22 14:03:03 -0400

    adding gitignore

--- orgInit.sh
+++ orgInit.sh
@@ -6,3 +6,4 @@ sfdx force:org:open -p /lightning/o/Course__c/list?filterName=All_Courses
 sfdx force:data:record:create -s CollaborationGroup -v "Name='Professional Translators' CollaborationType=Public Description='We translate anything you pay us to.  Any language, we can make it happen.  No fictional languages (Klingon, Pig Latin, Elvish, etc), please.' InformationTitle='How Translation Works' InformationBody='πώς λειτουργεί η μετάφραση'"
 sfdx shane:group:photo -f assets/rosetta.jpg -g "Professional Translators"
 sfdx shane:group:photo -b assets/rosettabanner.jpg -g "Professional Translators"
+sfdx force:org:open 
